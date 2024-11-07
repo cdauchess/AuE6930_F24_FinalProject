6359e3270da129cba757dd6e43147d5e832c0a93
cdauchess <cdauche@clemson.edu>
2024-11-06 22:40:50 -0500

    Sim interface dev (#1)
    * Updates and additions
    
    Corrected object references to Coppelia sim objects.
    Added a setSpeed and setSteering function for control of the sim
    
    Add some quick test lines to experiment with the Coppelia sim interface
    
    * Update readme, Added todo list, starting path info retrieval
    
    Beginning of path info retrieval. More work to be done in parsing the raw data of points along the path.
    Updated Readme to include basic instructions.
    
    * Path updates
    
    Now loading a number of paths from the environment. Using the path information to determine the position error of the vehicle relative to the active path.
    Updated README to provide instruction on how to name/structure additional paths in the simulation environment
    Updated simulation environment with "new" path naming/structure.
    
    * Simulation Environment, Steering Updates
    
    Updated steering functions to have a maximum steering angle to go along with the change to the "Manta" vehicle in Coppelia sim.
    Moved test script to a separate file. Implemented a simple steering controller for testing of the bridge.
    Updated the Coppelia Simulation environment to the Manta vehicle, and added cameras to watch the vehicle.
    
    * Hide motorbike in Coppelia Sim
    
    * Orientation Error Work
    
    Orientation error is a WIP. A coordinate frame issue currently exists.
    Updated Pathinformation.png to reflect current simulation structure.

--- CoppeliaBridge/CoppeliaBridge.py
+++ CoppeliaBridge/CoppeliaBridge.py
@@ -1,22 +1,59 @@
 #  python -m pip install coppeliasim-zmqremoteapi-client
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+import numpy as np
+import math
 
+'''
+TODO:
+Path Error Determination:
+    Orientation Error. There is currently a coordinate system issue here.
+    
+
+Obstacle Positions:
+    Return distance vectors to obstacles "in view"
+    Return contact boolean
+'''
+
+
+# This class is a bridge between Coppelia Sim and Python
 class CoppeliaBridge:
-    def __init__(self):
+    def __init__(self,numPaths):
         self._initialized = True
         self._isRunning = False
         
         self._client = RemoteAPIClient()        
-        self._sim = client.require('sim')
+        self._sim = self._client.require('sim')
 
         self._sim.setStepping(True)
+        
+        self._world = self._sim.getObject('/Floor')
+        
+        #These are the handles for the motorbike in the simulation environment
+        #self._egoVehicle = self._sim.getObject('/Motorbike/CoM')
+        #self._speedMotor = self._sim.getObject('/Motorbike/rearSuspension/motor')
+        #self._steerMotor = self._sim.getObject('/Motorbike/steeringMotor')
+        #These are the handles for the "Manta Vehicle"
+        self._egoVehicle = self._sim.getObject('/Manta/body_dummy')
+        self._speedMotor = self._sim.getObject('/Manta/motor_joint')
+        self._steerMotor = self._sim.getObject('/Manta/steer_joint')
+        
+        #Path information loading from simulation environment
+        self._path = []
+        self._pathPositions = []
+        self._pathQuaternions = []
+        self._pathLengths = []
+        for idx in range(numPaths):
+            pathPath = '/path' + str(idx+1)
+            self._path.append(self._sim.getObject(pathPath))
+            tempPos, tempQuaternion, tempLen = self.getPathData(idx) #Load Path Data from Model
+            self._pathPositions.append(tempPos)
+            self._pathQuaternions.append(tempQuaternion)
+            self._pathLengths.append(tempLen)
+            
+        self.activePath = 0 #Current path being followed by the vehicle
+        self.setMaxSteerAngle()
 
-        self._world = self._sim.getObject('../Floor')
-        self._egoVehicle = self._sim.getObject('../CoM')
-        self._speedMotor = self._sim.getObject('../motor')
-        self._steerMotor = self._sim.getObject('../steeringMotor')
-
-        pos = sim.getObjectPosition(C)
+        #pos = self._sim.getObjectPosition(C)
 
     def startSimulation(self):
         '''
@@ -39,18 +76,24 @@ class CoppeliaBridge:
         self._sim.stopSimulation()
         self._isRunning = False
 
-    
-    def getEgoPose(self):
-        pos = self._sim.getObjectPosition(self._egoVehicle)
-        rot = self._sim.getObjectOrientation(self._egoVehicle, self._world)
+    def getEgoPose(self, frame):
+        '''
+        Returns the vehicle's pose in a given frame
+        '''
+        pos = self._sim.getObjectPosition(self._egoVehicle, frame)
+        rot = self._sim.getObjectOrientation(self._egoVehicle, frame)
 
-        return [pos[1], pos[2], rot[2]]
+        return pos,rot
+    
+    def getEgoPoseWorld(self):
+        
+        return self.getEgoPose(self._world)
     
     def getTimeStepSize(self):
         '''
         Gets step size set in CoppeliaSim simulator
         '''
-        return self._sim.getTimeStepSize()
+        return self._sim.getSimulationTimeStep()
     
     def stepTime(self):
         '''
@@ -64,11 +107,108 @@ class CoppeliaBridge:
         Get current time in simulation
         '''
         return self._sim.getSimulationTime()
+    
+    def setSpeed(self,speedTarget):
+        '''
+        Set the motor speed
+        '''
+        self._sim.setJointTargetForce(self._speedMotor, 60) #Setting motor torque
+        self._sim.setJointTargetVelocity(self._speedMotor,speedTarget)
+        
+    def getSpeed(self):
+        '''
+        Get the current motor speed
+        '''
+        return self._sim.getJointVelocity(self._speedMotor)
 
+    def setSteering(self,steerTarget):
+        '''
+        Set the steering target
+        '''
+        #Bound the steering angle
+        if steerTarget>self._maxSteerAngle:
+            steerTarget = self._maxSteerAngle
+        elif steerTarget<(-1*self._maxSteerAngle):
+            steerTarget = -1*self._maxSteerAngle
 
+        self._sim.setJointTargetPosition(self._steerMotor,steerTarget)
+        
+    def getSteeringAngle(self):
+        '''
+        Get the current steering angle
+        '''
+        return self._sim.getJointPosition(self._steerMotor)
+        
+    def setMaxSteerAngle(self,maxAngle = 0.523599):
+        '''
+        Set the maximum steering angle allowed, in radians
+        '''
+        self._maxSteerAngle = maxAngle
 
+    #Path Reference: https://manual.coppeliarobotics.com/en/paths.htm
+    def getPathData(self, pathNum):
+        '''
+        Get the information about the chosen path
+        '''
+        #Assuming Path info is a list of position+quaterion (7 items per point) 
+        self.rawPathInfo = self._sim.unpackDoubleTable(self._sim.readCustomBufferData(self._path[pathNum],'PATH'))
+        pathInfo = np.reshape(self.rawPathInfo,(-1,7))
+        pathPositions = pathInfo[:, :3].flatten().tolist() #XYZ Positions of each path point
+        pathQuaternions = pathInfo[:, 3:].flatten().tolist() #Path point orientations
+        pathLengths, self.totalLength = self._sim.getPathLengths(pathPositions, 3) #Length along path for each path point
+        
+        return pathPositions, pathQuaternions, pathLengths
+        
+    
+    def getPathError(self, pathNum):
+        '''
+        Return the error relative to the closest point along the target path
+        Returns orientation error as the rotation error about the z axis.
+        '''
+        #Get the path info of the path in question
+        pathPos = self._pathPositions[pathNum]
+        pathQuaternion = self._pathQuaternions[pathNum]
+        pathLen = self._pathLengths[pathNum]
+        
+        currPos,currOrient = self.getEgoPose(self._path[self.activePath]) #Find the vehicle's current pose
+        posAlongPath = self._sim.getClosestPosOnPath(pathPos, pathLen, currPos)
+        nearestPoint = self._sim.getPathInterpolatedConfig(pathPos, pathLen, posAlongPath)#Convert the position along path to an XYZ position in the path's frame
+        pathError = np.subtract(currPos,nearestPoint)
 
-
+        #Find Orientation Error relative to the chosen path
+        #Currently a coordinate system issue in orientation error. 
+        lookAhead = 0.1
+        nextPos = posAlongPath+lookAhead
+        #Compare to the final path position, wrap around if beyond that
+        if nextPos > pathPos[-1]:
+            nextPos = pathPos[0]
+        nextNearestPoint = self._sim.getPathInterpolatedConfig(pathPos, pathLen, nextPos)
+        
+        pathVector = np.subtract(nextNearestPoint,nearestPoint)
+        pathTrajectory = math.tan(pathVector[1]/pathVector[0])
+        orientErr = currOrient[2]-pathTrajectory
+        orientErr = currOrient[2]+math.pi
+        
+        orientErr = None
+        
+        return pathError,orientErr
+    
+    def getVehicleState(self):
+        '''
+        Function to package the current vehicle state into a dictionary
+        '''
+        pos,orient = self.getEgoPoseWorld()
+        speed = self.getSpeed()
+        steer = self.getSteeringAngle()
+        
+        vehState = {
+            "Position": pos,
+            "Orientation": orient,
+            "Speed": speed,
+            "Steering": steer}
+        
+        return vehState
+    
     def __del__(self):
         '''
         Destructor
