7ed39e34e3ba4c754cbd8242e42e7ba135eb7719
pkorrapati <pranav.korrapati@gmail.com>
2024-11-14 09:54:17 -0500

    looks complete

--- CoppeliaBridge/CoppeliaBridge.py
+++ CoppeliaBridge/CoppeliaBridge.py
@@ -1,7 +1,8 @@
 #  python -m pip install coppeliasim-zmqremoteapi-client
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 import numpy as np
-from math import pi, tan, atan2, radians
+import matplotlib.pyplot as plt
+from math import pi, tan, atan2, radians, remainder
 
 '''
 TODO:
@@ -33,13 +34,13 @@ class CoppeliaBridge:
         self._m = 2.57                  # mass [Kg]
         self._r = 0.033                 # tire radius [m]        
         
-        self._V_MAX = 8 #3                 # max speed [m/s]
-        self._ACC_MAX = 5 #1.5             # max acceleration [m/s2]
+        self._V_MAX = 3                 # max speed [m/s]
+        self._ACC_MAX = 5               # max acceleration [m/s2]
         self._D_MAX = radians(26)       # max steering angle
 
         self._l = 0.258                 # wheel Base [m]
         self._t = 0.157                 # track width
-        self._tf = 0.112               # front king pin width
+        self._tf = 0.112                # front king pin width
 
         self._a = 0.126                 # distance from CG to front axle
         self._b = self._l - self._a     # distance from CG to rear axle
@@ -50,36 +51,47 @@ class CoppeliaBridge:
         
 
         self._ego = self._sim.getObject('/qcar/CoM')
+
         self._lDriveWheel = self._sim.getObject('/qcar/base_wheelrl_joint')
         self._rDriveWheel = self._sim.getObject('/qcar/base_wheelrr_joint')        
+
         self._lSteerAxis = self._sim.getObject('/qcar/base_hubfl_joint')
         self._rSteerAxis = self._sim.getObject('/qcar/base_hubfr_joint')    
-
-        
 
         self._isEgoReady = True  
 
         self.setSteering(self._d) 
         self.setVehicleSpeed(self._v) 
 
-    def initScene(self, numPaths):
-        #Path information loading from simulation environment
-        self._path = []
-        self._pathPositions = []
-        self._pathQuaternions = []
-        self._pathLengths = []
-        for idx in range(numPaths):
-            pathPath = '/path' + str(idx+1)
-            self._path.append(self._sim.getObject(pathPath))
-            tempPos, tempQuaternion, tempLen = self.getPathData(idx) #Load Path Data from Model
-            self._pathPositions.append(tempPos)
-            self._pathQuaternions.append(tempQuaternion)
-            self._pathLengths.append(tempLen)
+    # def initScene(self, numPaths):
+    #     #Path information loading from simulation environment
+    #     self._path = []
+    #     self._pathPositions = []
+    #     self._pathQuaternions = []
+    #     self._pathLengths = []
+    #     for idx in range(numPaths):
+    #         pathPath = '/path' + str(idx+1)
+    #         self._path.append(self._sim.getObject(pathPath))
+    #         tempPos, tempQuaternion, tempLen = self.getPathData(idx) #Load Path Data from Model
+    #         self._pathPositions.append(tempPos)
+    #         self._pathQuaternions.append(tempQuaternion)
+    #         self._pathLengths.append(tempLen)
             
-        self.activePath = 0 #Current path being followed by the vehicle
-        self.setMaxSteerAngle()
+    #     self.activePath = 0 #Current path being followed by the vehicle        
 
-        #pos = self._sim.getObjectPosition(C)
+    def initScene(self):
+        self._lanes = []
+        self._currentLane = -1
+
+        for i in range(10):
+            lane = self._sim.getObject("/path" + str(i + 1), {'noError' : True})
+            if lane >= 0:
+                self._lanes.append(lane)
+            else: 
+                break            
+
+        self._laneCount = len(self._lanes)
+        self.switchLane(0)        
 
     # Section: Application behavior
     def startSimulation(self):
@@ -128,12 +140,15 @@ class CoppeliaBridge:
         '''
         Get the current steering angle
         '''
+        d = 0
         dl = self._sim.getJointPosition(self._lSteerAxis)
-        sign = pow(-1, dl<0)        
         
-        R = (self._l / tan(abs(dl))) + self._tf/2
+        if dl != 0:
+            sign = pow(-1, dl<0)                
+            R = (self._l / tan(abs(dl))) + self._tf/2
+            d = sign * atan2(self._l, R)
 
-        return sign * atan2(self._l, R)
+        return d
 
     def setSteering(self, d):
         '''
@@ -156,8 +171,6 @@ class CoppeliaBridge:
         self._sim.setJointTargetPosition(self._rSteerAxis, dr)
 
         self._adjustDifferential()
-
-        # self._sim.setJointTargetPosition(self._steerMotor,steerTarget)    
     
     def getEgoPoseRelative(self, frame):
         '''
@@ -171,6 +184,15 @@ class CoppeliaBridge:
     def getEgoPoseAbsolute(self):        
         return self.getEgoPoseRelative(self._world)
     
+    def getVehicleSpeed(self):
+        '''
+        Get the current motor speed
+        '''
+        vl = -self._sim.getJointVelocity(self._lDriveWheel)
+        vr =  self._sim.getJointVelocity(self._rDriveWheel)
+        
+        return((vl + vr)/2)        
+    
     def setVehicleSpeed(self, v):
         '''
         Set the vehicle speed in m/s
@@ -178,23 +200,22 @@ class CoppeliaBridge:
         # self._sim.setJointTargetForce(self._speedMotor, 60) #Setting motor torque
         self._v = helper.bound(v, 0, self._V_MAX)
         self._adjustDifferential()
-        
-    def getVehicleSpeed(self):
-        '''
-        Get the current motor speed
-        '''
-        print(self._sim.getJointVelocity(self._lDriveWheel))
-        print(self._sim.getJointVelocity(self._rDriveWheel))
-        print(" ")
-        # return self._sim.getJointVelocity(self._speedMotor)
-
+    
     def setMotion(self, throttle):
+        throttle = helper.bound(throttle, -1.0, 1.0)                
         self.setVehicleSpeed(self._v + (throttle * self._ACC_MAX * self._timeStep))
-        print(self._v)
+    
+    def switchLane(self, laneNumber):             
+        if self._laneCount > 0:
+            if (laneNumber >= 0) and (laneNumber < self._laneCount):
+                self._currentLane = laneNumber
+        else:
+            self._currentLane = -1
 
-        # f = m * a * self._accLimit
-        # t * f * r/2
-        
+        rawPathInfo = self._sim.unpackDoubleTable(self._sim.readCustomBufferData(self._lanes[self._currentLane], 'PATH'))
+        # self._pathPts = (np.reshape(rawPathInfo, (-1,7)))[:, :3]
+        self._pathPoints = (np.reshape(rawPathInfo, (-1,7)))[:, :3].flatten().tolist()
+        self._pathLengths, self._totalLength = self._sim.getPathLengths(self._pathPoints, 3)    
 
     def _adjustDifferential(self):
         '''
@@ -203,78 +224,56 @@ class CoppeliaBridge:
         vl = self._v
         vr = self._v
 
-        # if self._d != 0:
-        #     d_abs = abs(self._d)        
-        #     sign = pow(-1, self._d < 0) # -ve indicates right turn
+        if self._d != 0:
+            d_abs = abs(self._d)        
+            sign = pow(-1, self._d < 0) # -ve indicates right turn
             
-        #     R = (self._l / tan(d_abs))
+            R = (self._l / tan(d_abs))
 
-        #     vl = self._v * (R - (sign * self._t/2)) / R
-        #     vr = self._v * (R + (sign * self._t/2)) / R
+            vl = self._v * (R - (sign * self._t/2)) / R
+            vr = self._v * (R + (sign * self._t/2)) / R
 
         self._sim.setJointTargetVelocity(self._lDriveWheel, -vl/self._r)
         self._sim.setJointTargetVelocity(self._rDriveWheel,  vr/self._r)
-
-        # print(" ----- ")
-        # print(vl/self._r)
-        # print(vr/self._r)
-        # print(self._sim.getJointVelocity(self._lDriveWheel))
-        # print(self._sim.getJointVelocity(self._rDriveWheel))
-    
-    #Path Reference: https://manual.coppeliarobotics.com/en/paths.htm
-    def getPathData(self, pathNum):
-        '''
-        Get the information about the chosen path
-        '''
-        #Assuming Path info is a list of position+quaterion (7 items per point) 
-        self.rawPathInfo = self._sim.unpackDoubleTable(self._sim.readCustomBufferData(self._path[pathNum],'PATH'))
-        pathInfo = np.reshape(self.rawPathInfo,(-1,7))
-        pathPositions = pathInfo[:, :3].flatten().tolist() #XYZ Positions of each path point
-        pathQuaternions = pathInfo[:, 3:].flatten().tolist() #Path point orientations
-        pathLengths, self.totalLength = self._sim.getPathLengths(pathPositions, 3) #Length along path for each path point
         
-        return pathPositions, pathQuaternions, pathLengths
-        
-    
-    def getPathError(self, pathNum):
+    def getPathError(self):
         '''
         Return the error relative to the closest point along the target path
         Returns orientation error as the rotation error about the z axis.
         '''
-        #Get the path info of the path in question
-        pathPos = self._pathPositions[pathNum]
-        pathQuaternion = self._pathQuaternions[pathNum]
-        pathLen = self._pathLengths[pathNum]
+        #Get the path info of the current path
+        pathPos = self._pathPoints     
+        pathLen = self._pathLengths
         
-        currPos,currOrient = self.getEgoPose(self._path[self.activePath]) #Find the vehicle's current pose
-        posAlongPath = self._sim.getClosestPosOnPath(pathPos, pathLen, currPos)
-        nearestPoint = self._sim.getPathInterpolatedConfig(pathPos, pathLen, posAlongPath)#Convert the position along path to an XYZ position in the path's frame
-        pathError = np.subtract(currPos,nearestPoint)
+        pos, rot = self.getEgoPoseAbsolute() #Find the vehicle's current pose
 
-        #Find Orientation Error relative to the chosen path
-        #Currently a coordinate system issue in orientation error. 
+        posAlongPath = self._sim.getClosestPosOnPath(pathPos, pathLen, pos)
+        nearPt = self._sim.getPathInterpolatedConfig(pathPos, pathLen, posAlongPath) #Convert the position along path to an XYZ position in the path's frame
+        
+        pathError = np.subtract(pos, nearPt)
+
         lookAhead = 0.1
-        nextPos = posAlongPath+lookAhead
+        nextPos = posAlongPath + lookAhead
+        
         #Compare to the final path position, wrap around if beyond that
-        if nextPos > pathPos[-1]:
-            nextPos = pathPos[0]
-        nextNearestPoint = self._sim.getPathInterpolatedConfig(pathPos, pathLen, nextPos)
+        if nextPos > self._totalLength:
+            nextPos = remainder(nextPos, self._totalLength)
         
-        pathVector = np.subtract(nextNearestPoint,nearestPoint)
-        pathTrajectory = tan(pathVector[1]/pathVector[0])
-        orientErr = currOrient[2] - pathTrajectory
-        orientErr = currOrient[2] + pi
+        nextNearPt = self._sim.getPathInterpolatedConfig(pathPos, pathLen, nextPos)
         
-        orientErr = None
-        
-        return pathError,orientErr
+        pathVector = np.subtract(nextNearPt, nearPt)
+        pathTrajectory = atan2(pathVector[1], pathVector[0])
+        orientErr =  helper.pipi(pathTrajectory - rot[2] + pi)
+                
+        # print(orientErr)
+        return pathError, orientErr
     
     def getVehicleState(self):
         '''
         Function to package the current vehicle state into a dictionary
         '''
         pos, rot = self.getEgoPoseAbsolute()
-        v = self.getSpeed()
+        v = self.getVehicleSpeed()
         d = self.getSteeringAngle()
         
         X = {"Position": pos[0:1],
@@ -295,7 +294,10 @@ class CoppeliaBridge:
             self._initialized = False
         pass
 
-
 class helper:
     def bound(a, aMin, aMax):
         return max(min(a, aMax), aMin)
+    
+    def pipi(a): 
+        a = remainder(a, 2*pi)       
+        return (2*pi - a) if a > pi else a
--- bridgeTest.py
+++ bridgeTest.py
@@ -5,25 +5,28 @@ from math import sin, pi, degrees
 bridge = CoppeliaBridge()
 
 bridge.initEgo()
+bridge.initScene()
 
 bridge.startSimulation()
+# print(bridge.getPathError())
 
-# bridge.getPaths()
-
-# bridge.setVehicleSpeed(0)
-# bridge.setSteering(0.4)
+bridge.setVehicleSpeed(0.6)
+# bridge.setSteering(0.45)
 
 curTime = 0
-runTime = 4
+runTime = 30
 
 while bridge._isRunning and (curTime < runTime):    
     bridge.stepTime()
     curTime = bridge.getTime()
-    bridge.setMotion(1)
+    # bridge.setMotion(1)
     # bridge.getEgoPoseAbsolute()
 
     # bridge.getVehicleSpeed()
     # bridge.setSteering(0.2 * sin(4*pi*curTime/runTime))
+    p,o = bridge.getPathError()
+    # print(o)
+    bridge.setSteering(-0.4*o)
 
 bridge.setVehicleSpeed(0)
 bridge.stopSimulation()
